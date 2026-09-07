a378087f3f6c62ce41cabb65b95cc4461c85600b
Suvrat Garg <suvrat.garg@gmail.com>
2026-09-07 07:03:32 +0530

    Remove redundant token import in rail catalog previews

--- widgetbook/lib/primitives/rail_use_cases.dart
+++ widgetbook/lib/primitives/rail_use_cases.dart
@@ -1,4 +1,3 @@
-import 'package:catch_tokens/catch_tokens.dart';
 import 'package:catch_ui/catch_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
