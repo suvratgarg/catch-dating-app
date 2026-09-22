6958dda143cb1fdcc079b2029d22c7d8995d9884
Suvrat Garg <suvrat.garg@gmail.com>
2026-09-22 23:34:12 +0530

    Keep lifecycle capture imports in analyzer order

--- test/ui_captures/audience_directory_preview_test.dart
+++ test/ui_captures/audience_directory_preview_test.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:catch_ui/catch_ui.dart';
 import 'package:catch_dating_app/hosts/domain/host_application_summary.dart';
+import 'package:catch_ui/catch_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
